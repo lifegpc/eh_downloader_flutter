ba77b72d46072f77b4579ae1c3c071971a7f96b7
lifegpc <g1710431395@gmail.com>
2023-08-28 13:04:36 +0800

    Add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# eh_downloader_flutter
+
+Flutter Frontend for [EH Downloader](https://github.com/lifegpc/eh-downloader)
---
+++ android/app/src/main/kotlin/com/example/eh_downloader_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eh_downloader_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
