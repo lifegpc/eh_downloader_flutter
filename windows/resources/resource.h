b4bf8640d7c6dd333c1d2ba608641ab2666de36b
lifegpc <root@lifegpc.com>
2024-02-17 08:35:23 +0800

    Add Windows resources

---
+++ windows/resources/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 resources.rc 使用
+//
+#define IDS_JPEG                        101
+#define IDS_PNG                         102
+#define IDS_GIF                         103
+#define IDR_VERSION2                    103
+#define IDS_ZIP                         104
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
